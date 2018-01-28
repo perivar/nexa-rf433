35c8074b05986f451c88bf1736be8bb8b056cd14
Per Ivar Nerseth <perivar@nerseth.com>
2018-01-28 11:34:06 +0100

    Added interrupt driven receiver taken from RC-Switch and heaviliy modified

--- src/nexa_receiver.ino
+++ src/nexa_receiver.ino
@@ -1,25 +1,60 @@
 /**
- * Modified https://playground.arduino.cc/Code/HomeEasy?action=sourceblock&num=3
+ * Parts modified https://playground.arduino.cc/Code/HomeEasy?action=sourceblock&num=3
  * by
  * Barnaby Gray 12/2008
  * Peter Mead   09/2009
+ * Other parts taken from RC-Switch make it Nexa compatible
  */
 
 #include <Arduino.h>
+
+#define DEBUG //If you comment this line, debug statements are turned off
 
 // function prototype allowing the function to be used before it is defined.
 static char *dec2binWzerofill(unsigned long dec, unsigned int bitLength);
 void printBinary(unsigned long inNumber, unsigned int digits = 32);
 
+// RC-Switch variables
+#define RCSWITCH_MAX_CHANGES 250
+const unsigned int nSeparationLimit = 4300;
+unsigned int timings[RCSWITCH_MAX_CHANGES];
+volatile char *receivedBitsArray = new char[RCSWITCH_MAX_CHANGES];
+volatile unsigned int changeCount = 0;
+unsigned long nReceivedValue = NULL;
+unsigned int nReceivedBitlength = 0;
+unsigned int nReceivedDelay = 0;
+unsigned int nReceivedProtocol = 0;
+int nReceiveTolerance = 60;
+
+// input pin for the 433 Mhz module
 int rxPin = 2;
 
 void setup()
 {
-    pinMode(rxPin, INPUT);
     Serial.begin(9600);
+    pinMode(rxPin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(rxPin), interrupt_handler, CHANGE); // 0 is D2
 }
 
 void loop()
+{
+    //loop_locked();
+
+    if (available())
+    {
+        Serial.print("Received ");
+        Serial.print(nReceivedValue);
+        Serial.print(" / ");
+        Serial.print(nReceivedBitlength);
+        Serial.print("bit ");
+        Serial.print("Protocol: ");
+        Serial.println(nReceivedProtocol);
+
+        resetAvailable();
+    }
+}
+
+void loop_locked()
 {
     int i = 0;
     unsigned long t = 0;
@@ -119,6 +154,207 @@ void loop()
     }
 }
 
+static inline unsigned int diff(int A, int B)
+{
+    return abs(A - B);
+}
+
+bool receiveProtocolNexa(unsigned int changeCount)
+{
+    // there are no Nexa protocols with less than x bits
+    // ignore very short transmissions: no device sends them, so this must be noise
+    if (changeCount < 8)
+        return false;
+
+    unsigned long code = 0;
+    unsigned long delay = timings[0] / 41;
+    unsigned long delayTolerance = delay * nReceiveTolerance * 0.01;
+
+    // store bits in char array
+    unsigned int nReceivedBitsPos = 0;
+    unsigned int nTmpReceivedBitlength = (changeCount - 3) / 2; // 3 startings bits
+
+    // dynamically define the bit char array
+    // https://arduino.stackexchange.com/questions/3774/how-can-i-declare-an-array-of-variable-size-globally
+    //if (receivedBitsArray != 0)
+    //{
+    //  delete[] receivedBitsArray;
+    //}
+    //receivedBitsArray = new char[nTmpReceivedBitlength];
+
+#ifdef DEBUG
+    Serial.println();
+    Serial.print("Detecting protocol using ");
+    Serial.print(changeCount);
+    Serial.print(" timings. Delay: ");
+    Serial.print(delay);
+    Serial.print(". Delay tolerance: ");
+    Serial.print(delayTolerance);
+    Serial.print(". Bit length: ");
+    Serial.print(nTmpReceivedBitlength);
+    Serial.println();
+
+    Serial.print("Raw data: ");
+    for (int i = 0; i <= changeCount; i++)
+    {
+        Serial.print(timings[i]);
+        Serial.print(",");
+    }
+    Serial.println();
+#endif
+
+    for (int i = 1; i < changeCount; i = i + 2)
+    {
+        // check if have sync bit (T + 10T)
+        if (timings[i] > delay - delayTolerance && timings[i] < delay + delayTolerance && timings[i + 1] > delay * 10 - delayTolerance && timings[i + 1] < delay * 11 + delayTolerance)
+        {
+#ifdef DEBUG
+            Serial.print("sync ");
+#endif
+        }
+        // or 1 bit (T + T)
+        else if (timings[i] > delay - delayTolerance && timings[i] < delay + delayTolerance && timings[i + 1] > delay - delayTolerance && timings[i + 1] < delay + delayTolerance)
+        {
+#ifdef DEBUG
+            Serial.print("1");
+#endif
+            receivedBitsArray[nReceivedBitsPos++] = '1';
+        }
+        // or 0 bit (T + 5T)
+        else if (timings[i] > delay - delayTolerance && timings[i] < delay + delayTolerance && timings[i + 1] > delay * 5 - delayTolerance && timings[i + 1] < delay * 5 + delayTolerance)
+        {
+#ifdef DEBUG
+            Serial.print("0");
+#endif
+            receivedBitsArray[nReceivedBitsPos++] = '0';
+        }
+        else
+        {
+            // Failed
+            i = changeCount;
+
+            // reset bits array
+            delete[] receivedBitsArray;
+            nReceivedBitsPos = 0;
+        }
+    }
+
+#ifdef DEBUG
+    if (nReceivedBitsPos > 0)
+    {
+        Serial.println();
+        Serial.print("verf ");
+        for (int j = 0; j < nReceivedBitsPos; j++)
+        {
+            Serial.print(receivedBitsArray[j]);
+        }
+        Serial.println();
+    }
+#endif
+
+    // Decode the data stream into logical bytes.
+    // The data part on the physical link is coded so that every logical bit is sent as
+    // two physical bits, where the second one is the inverse of the first one.
+    // '0' => '01'
+    // '1' => '10'
+    // Example the logical datastream 0111 is sent over the air as 01101010.
+    // I.e. keep every second byte
+    if (nReceivedBitsPos > 0)
+    {
+        for (int k = 0; k < nReceivedBitsPos; k = k + 2)
+        {
+            code <<= 1;
+            if (receivedBitsArray[k] == '1')
+            {
+                code |= 1;
+            }
+        }
+    }
+
+#ifdef DEBUG
+    if (code > 0)
+    {
+        Serial.print("lgic ");
+        Serial.println(dec2binWzerofill(code, nTmpReceivedBitlength / 2));
+    }
+#endif
+
+    // ignore low bit counts as there are no devices sending only x bit values => noise
+    if (changeCount > 8)
+    {
+        nReceivedValue = code;
+        nReceivedBitlength = nTmpReceivedBitlength;
+        nReceivedDelay = delay;
+        nReceivedProtocol = 10;
+    }
+
+    if (code == 0)
+    {
+        return false;
+    }
+    else if (code != 0)
+    {
+        return true;
+    }
+}
+
+bool available()
+{
+    return nReceivedValue != NULL;
+}
+
+void resetAvailable()
+{
+    nReceivedValue = NULL;
+}
+
+void interrupt_handler()
+{
+    static unsigned int duration = 0;
+    static unsigned int changeCount = 0;
+    static unsigned long lastTime = 0;
+    static unsigned int repeatCount = 0;
+
+    long curTime = micros();
+    duration = curTime - lastTime;
+
+    if (duration > nSeparationLimit)
+    {
+        // A long stretch without signal level change occurred. This could
+        // be the gap between two transmission.
+        if (diff(duration, timings[0]) < 200)
+        {
+            // This long signal is close in length to the long signal which
+            // started the previously recorded timings; this suggests that
+            // it may indeed by a a gap between two transmissions (we assume
+            // here that a sender will send the signal multiple times,
+            // with roughly the same gap between them).
+            repeatCount++;
+            changeCount--;
+            if (repeatCount == 2)
+            {
+                if (receiveProtocolNexa(changeCount) == false)
+                {
+                    // failed
+                }
+                repeatCount = 0;
+            }
+        }
+        changeCount = 0;
+    }
+
+    // detect overflow
+    if (changeCount >= RCSWITCH_MAX_CHANGES)
+    {
+        changeCount = 0;
+        repeatCount = 0;
+    }
+
+    timings[changeCount++] = duration;
+    lastTime = curTime;
+}
+
+// print results from the locked read loop
 void printResult(unsigned long sender, unsigned long senderbin1, unsigned long senderbin2, bool group, bool on, unsigned int command)
 {
     Serial.print("sender ");
@@ -154,14 +390,8 @@ void printResult(unsigned long sender, unsigned long senderbin1, unsigned long s
     Serial.println();
 }
 
-void printBinary(unsigned long inNumber, unsigned int digits = 32)
+static void printBinary(unsigned long inNumber, unsigned int digits = 32)
 {
-    /*
-  for (int b = digits-1; b >= 0; b--)
-  {
-    Serial.print(bitRead(inNumber, b));
-  }
-  */
     Serial.print(dec2binWzerofill(inNumber, digits));
 }
 
